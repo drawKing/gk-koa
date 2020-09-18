3793ab6b87cc0013564094714d0b1f250d593155
gaoheming <gao_hm@suixingpay.com>
2020-09-18 09:12:59 +0800

    容错处理 🌟

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 import Koa from 'koa';
 import koaBody from "koa-body";
+import log4js from "log4js";
+import errorHandler from './middlewares/errorHandlers';
 import router from "./route";
 import './controllers';
 
 const app = new Koa();
 
+// 错误日志记录
+log4js.configure({
+    appenders: {globalError: {type: "file", filename: "./logs/error.log"}},
+    categories: {default: {appenders: ["globalError"], level: "error"}},
+});
+
 app.use(koaBody(
     {multipart: true}
 ));
+
+// 容错处理
+const logger = log4js.getLogger("cheese");
+errorHandler.error(app, logger);
+
 app.use(router.routes())
     .use(router.allowedMethods());
 
---
+++ middlewares/errorHandlers.js
@@ -0,0 +1,26 @@
+// 错误处理
+export default class ErrorHandlers {
+    static error(app, logger) {
+        // 全局错误 catch
+        app.use(async (ctx, next) => {
+            try {
+                await next();
+            } catch (e) {
+                console.log(e, 'eeee');
+                // 错误日志记录
+                logger.error(e);
+                ctx.body = "500 请求，正在积极修复";
+            }
+        });
+
+        // 处理页面的 404 401 等
+        app.use(async (ctx, next) => {
+            await next();
+            if (ctx.status === 404) {
+                // 公益 404
+                ctx.body = `<script type="text/javascript" src="//qzonestyle.gtimg.cn/qzone/hybrid/app/404/search_children.js" charset="utf-8"></script>`;
+            }
+        });
+    }
+}
+
