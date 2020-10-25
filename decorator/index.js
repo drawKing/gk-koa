524e47b03bc68292a6dfa158a32f1c6836b14dec
gaoheming <gao_hm@suixingpay.com>
2020-10-25 10:28:27 +0800

    整理 装饰器

--- decorator/index.js
+++ decorator/index.js
@@ -9,13 +9,13 @@ const Methods = {
 // 包装接口真实路径，及用到的中间件
 function prefix(prefix) {
     return function (target) {
-        const proto = Object.getOwnPropertyNames(target.prototype);
+        const proto = Object.getOwnPropertyNames(target.prototype); // 返回所有自身上的属性名 [ 'constructor', 'showIndex', 'login', 'getData' ]
 
         for (let key of proto) {
             const path = Reflect.getMetadata('path', target.prototype, key);
             const method = Reflect.getMetadata('method', target.prototype, key);
             const middleware = Reflect.getMetadata('middleware', target.prototype, key);
-            const handler = target.prototype[key];
+            const handler = target.prototype[key]; // 传给 router 的回调函数
 
             if (path && method) {
                 const fullPath = prefix === '/' ? path : `${prefix}${path}`;
@@ -33,6 +33,8 @@ function prefix(prefix) {
 function request(type) {
     return function (path) {
         return function (target, key) {
+            // console.log(target, 'target'); // 具体到某个controller: LoginController
+            // console.log(key, 'key'); // 具体方法名: getData
             Reflect.defineMetadata('path', path, target, key);
             Reflect.defineMetadata('method', type, target, key);
         }
